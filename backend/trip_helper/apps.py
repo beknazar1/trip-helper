8a794b51dd6af0cfabaf75754c0fcaf479cd3a77
Bek Zhuma <5906333+beknazar1@users.noreply.github.com>
2020-03-23 14:06:21 -0500

    Initial commit - Django and React config

---
+++ backend/trip_helper/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class TripHelperConfig(AppConfig):
+    name = 'trip_helper'
---
+++ backend/trip_helper/urls.py
@@ -0,0 +1,6 @@
+from django.urls import path
+from rest_framework.urlpatterns import format_suffix_patterns
+
+urlpatterns = format_suffix_patterns([
+
+])
