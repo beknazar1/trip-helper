aecff4ec856407c1bad5a7ee29c1d44d3a66e8c6
Bek Zhuma <5906333+beknazar1@users.noreply.github.com>
2020-03-27 23:00:58 -0500

    CRUD Api for Trips

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -4,8 +4,29 @@ const API = 'http://localhost:8000/api/'
 
 const django = axios.create({
   baseURL: API,
-  timeout: 1000,
+  timeout: 15000,
   headers: {'Authorization': `JWT ${localStorage.getItem('token')}`},
 })
 
 export const tripList = () => django.get('trips/')
+
+export const tripCreate = (data) => django.post('trips/', {
+  'name': data.name,
+  'scheduled_date': data.scheduledDate,
+  'origin_city': data.originCity,
+  'origin_state': data.originState,
+  'destination_city': data.destinationCity,
+  'destination_state': data.destinationState,
+})
+
+export const tripDetail = (tripId) => django.get(`trips/${tripId}/`).catch(err => err.response)
+
+export const tripUpdate = (data, tripId) => django.put(`trips/${tripId}/`, {
+  'name': data.name,
+  'scheduled_date': data.scheduledDate,
+  'origin_city': data.originCity,
+  'origin_state': data.originState,
+  'destination_city': data.destinationCity,
+  'destination_state': data.destinationState,
+})
+export const tripDelete = (tripId) => django.delete(`trips/${tripId}/`)
